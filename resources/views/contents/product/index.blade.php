4193fca1116a9efb977b77a38cd2ddeafcdb1199
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-07-27 17:23:25 +0700

    dashboard, kerangka product

---
+++ resources/views/contents/product/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('title')
+  Produk
+@endsection
+
+@section('content')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+  <h1 class="h2">Produk</h1>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('dashboard');
 });
+
+Route::view('dashboard', 'contents.dashboard.index')->name('dashboard');
+
+Route::resource('products', ProductController::class);
